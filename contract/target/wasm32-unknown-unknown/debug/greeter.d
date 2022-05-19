ed20aaf3be3a8213c3c4adf5279111bc929584fd
Admin <admin@admins-mbp.home>
2022-05-19 23:45:02 +0300

    init commit

---
+++ contract/target/wasm32-unknown-unknown/debug/greeter.d
@@ -0,0 +1,1 @@
+/Users/admin/Downloads/happy-amber-dev-master/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /Users/admin/Downloads/happy-amber-dev-master/contract/src/lib.rs
---
+++ contract/target/wasm32-unknown-unknown/debug/libgreeter.d
@@ -0,0 +1,1 @@
+/Users/admin/Downloads/happy-amber-dev-master/contract/target/wasm32-unknown-unknown/debug/libgreeter.rlib: /Users/admin/Downloads/happy-amber-dev-master/contract/src/lib.rs
