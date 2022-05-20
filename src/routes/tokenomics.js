5490ffb7676928a596055e8c1e8cf8b91f893498
Admin <admin@admins-mbp.home>
2022-05-20 23:46:24 +0300

    added tokenomics google doc

--- src/routes/tokenomics.js
+++ src/routes/tokenomics.js
@@ -8,7 +8,7 @@ export default function Tokenomics() {
         <Header/>
       <main style={{ padding: "1rem 0" }}>
         <h2>Tokenomics</h2>
-      </main>
+        <iframe className='w-full min-h-screen' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTluHyM4978mHAEXEOOSTgfR6zfuqjpbDapAwyN9zpQ_4x_UYRgxbYS4v8gyR_QLTAQxtR0krBTTtHQ/pubhtml?widget=true&amp;headers=false"></iframe>      </main>
       </>
     );
   }
